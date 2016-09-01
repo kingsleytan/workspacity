79e967a271b2b46a63ce0f7aeba7b0b3a05c8266
gswerm <gswerm@gmail.com>
2016-09-01 12:11:30 +0800

    updates packages controller

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,30 +1,53 @@
 class PackagesController < ApplicationController
-  before_action :authenticate!, only: [:create, :edit, :update, :new, :destroy]
 
   def index
+    @packages = Package.all
   end
 
   def show
+    @package = Package.find_by(id: params[:id])
   end
 
   def new
+    @package = Package.new
   end
 
   def create
-      @packages = current_user.packages.build(topic_params)
-    if @packages.save
+    @package = Package.new(package_params)
+
+    if @package.save
       redirect_to packages_path
     else
-      redirect_to _path
+      redirect_to new_package_path
     end
   end
 
   def edit
+    @package = Package.find_by(id: params[:id])
   end
 
   def update
+    @package = Package.find_by(id: params[:id])
+
+    if @package.update(package_params)
+      redirect_to package_path(@package)
+    else
+      redirect_to edit_package_path(@package)
+    end
   end
 
   def destroy
+    @package = Package.find_by(id: params[:id])
+    if @package.destroy
+      redirect_to packages_path
+    else
+      redirect_to package_path(@package)
+    end
+  end
+
+  private
+
+  def package_params
+    params.require(:package).permit(:title, :description, :price, :location, :image, :service_id)
   end
 end
